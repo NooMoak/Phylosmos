6d1031bd4c1bd36f49939dfdb3fb4c9d9f06ac15
Eliot <ringevaleliot@hotmail.fr>
2019-03-04 14:33:08 +0100

    MaJ Eliot final project
    Maj Final-prject and Eliot scene

--- Phylosmos_Valid/Assets/ELR_Scripts/PlayerDamage.cs
+++ Phylosmos_Valid/Assets/ELR_Scripts/PlayerDamage.cs
@@ -5,27 +5,56 @@ using UnityEngine.UI;
 
 public class PlayerDamage : MonoBehaviour
 {
-    int PlayerHealth;
+    float PlayerHealth;
+    float fadeRate = 1.5f;
+    float targetAlpha;
     [SerializeField]
     Image healthBar;
+    [SerializeField]
+    Image redBorders;
 
     private void Start() 
     {
-        PlayerHealth = 100;
+        PlayerHealth = 100f;
+        targetAlpha = 0.7f;
+    }
+
+     private void Update() 
+    {
+        Color curColor = redBorders.color;
+        float alphaDiff = Mathf.Abs(curColor.a-targetAlpha);
+        if (alphaDiff>0.0001f)
+        {
+            curColor.a = Mathf.Lerp(curColor.a,targetAlpha,fadeRate*Time.deltaTime);
+            redBorders.color = curColor;
+        }
+        if(alphaDiff <0.0001f)
+        {
+            curColor.a = 0.7f;
+            redBorders.color = curColor;
+            targetAlpha = 0.7f;
+            redBorders.enabled = false;
+        }
     }
     private void OnTriggerEnter(Collider collision)
     {
 		if(collision.gameObject.CompareTag("SpikeProjectile"))
         {
             Destroy(collision.gameObject);
-            TakeDamage(10);
+            TakeDamage(10f);
         }
     }
 
-    void TakeDamage(int damage)
+    void TakeDamage(float damage)
     {
+        Color curColor = redBorders.color;
         PlayerHealth -= damage;
-        healthBar.fillAmount = PlayerHealth;
+        healthBar.fillAmount = PlayerHealth / 100;
+        curColor.a = 0.7f;
+        redBorders.color = curColor;
+        targetAlpha = 0.7f;
+        redBorders.enabled = true;
+        targetAlpha = 0.2f;
         CheckHealth();
     }
 
