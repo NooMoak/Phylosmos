df6ee7db85f185ccaa5ad1236316750ef57e8bcb
Eliot <ringevaleliot@hotmail.fr>
2019-05-08 17:23:51 +0200

    ELR_CamMoveTuto

---
+++ Phylosmos_Valid/Assets/ELR_Scripts/Camera_Script/IntroCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCam : MonoBehaviour
+{
+
+    [SerializeField] GameObject normalCam;
+    [SerializeField] GameObject introCam;
+    [SerializeField] GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.gameObject.GetComponent<CameraController>().exeption = true;
+        UI.SetActive(false);
+        StartCoroutine(CamMove());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CamMove()
+    {
+        yield return new WaitForSeconds(2);
+        introCam.SetActive(false);
+        Camera.main.gameObject.GetComponent<CameraController>().exeption = false;
+        Camera.main.gameObject.GetComponent<CameraController>().NormalCam();
+        UI.SetActive(true);
+    }
+}
