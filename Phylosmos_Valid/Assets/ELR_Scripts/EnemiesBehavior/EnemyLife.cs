574c605ea1015068d2dd6acf02974918b5fee06b
Eliot <ringevaleliot@hotmail.fr>
2019-05-21 13:17:30 +0200

    ELR_Update

--- Phylosmos_Valid/Assets/ELR_Scripts/EnemiesBehavior/EnemyLife.cs
+++ Phylosmos_Valid/Assets/ELR_Scripts/EnemiesBehavior/EnemyLife.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class EnemyLife : MonoBehaviour
 {
-    public int maxHealth;
-    public int health;
+    public float maxHealth;
+    public float health;
     public bool invicible;
     [SerializeField] GameObject healthBar;
+    [SerializeField] GameObject healthBarContainer;
     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
         anim = GetComponentInChildren<Animator>();
+        healthBar.transform.localScale = new Vector3(0.5f, 0.5f, maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.transform.rotation = Quaternion.Euler(0,-45,-30);
-        healthBar.GetComponentInParent<Transform>().localScale = new Vector3 (1, 1, health/maxHealth);
-        healthBar.transform.localScale = new Vector3(0.5f, 0.5f, maxHealth);
+        healthBar.transform.rotation = Quaternion.Euler(0,0,0);
+        healthBarContainer.transform.rotation = Quaternion.Euler(0,-45,-30);
+        float healthToDisplay = Mathf.Lerp(healthBarContainer.transform.localScale.z, health/maxHealth, 0.2f);
+        healthBarContainer.transform.localScale = new Vector3 (1, 1, healthToDisplay);
     }
 
     public void TakeDamage(int damage)
--- Phylosmos_Valid/Assets/ELR_Scripts/EnemiesBehavior/LianaBehavior.cs
+++ Phylosmos_Valid/Assets/ELR_Scripts/EnemiesBehavior/LianaBehavior.cs
@@ -39,7 +39,7 @@ public class LianaBehavior : MonoBehaviour
             StopCoroutine("Stunned");
             GetComponent<CapsuleCollider>().enabled = false;
             GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
-            transform.GetChild(1).GetComponent<MeshRenderer>().enabled = false;
+            GetComponentInChildren<MeshRenderer>().enabled = false;
             anim.SetBool("IsWalking", false);
             if(Vector3.Distance(player.transform.position, homePosition) < 120 && Vector3.Distance(player.transform.position, homePosition) > 100)
                 StartCoroutine("Respawn");
@@ -115,7 +115,7 @@ public class LianaBehavior : MonoBehaviour
         currentState = LianaState.Sleep;
         GetComponent<CapsuleCollider>().enabled = true;
         GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
-        transform.GetChild(1).GetComponent<MeshRenderer>().enabled = true;
+        GetComponentInChildren<MeshRenderer>().enabled = true;
     }
 
     IEnumerator Stunned()
