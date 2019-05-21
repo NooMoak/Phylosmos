d32c4bcfb083e361438c9aa8e9ad8a2cbef6a6f5
Eliot <ringevaleliot@hotmail.fr>
2019-05-21 19:53:43 +0200

    ELR_NewPlayer

---
+++ Phylosmos_Valid/Assets/ELR_Scripts/PlayerScripts/AnimEvents/PlayerAnimEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    public void PlayerAnimShoot()
+    {
+        GetComponentInParent<PlayerController>().Fire();
+    }
+}
